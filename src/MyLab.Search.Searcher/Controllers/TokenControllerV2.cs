018e0855877d89bebb5675ddf49f7a504ec5a319
Andrey O <ozzy.ext@gmail.com>
2023-01-11 22:10:58 +0300

    Return V2

---
+++ src/MyLab.Search.Searcher/Controllers/TokenControllerV2.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using MyLab.Search.Searcher.Models;
+using MyLab.Search.Searcher.Services;
+using MyLab.WebErrors;
+
+namespace MyLab.Search.Searcher.Controllers
+{
+    [ApiController]
+    [Route("v2/token")]
+    public class TokenControllerV2 : ControllerBase
+    {
+        private readonly ITokenService _tokenService;
+
+        public TokenControllerV2(ITokenService tokenService)
+        {
+            _tokenService = tokenService;
+        }
+
+        [HttpPost]
+        [ErrorToResponse(typeof(TokenizingDisabledException), HttpStatusCode.Locked)]
+        public IActionResult Post([FromBody] TokenRequestV2 tokenRequest)
+        {
+            return Ok(_tokenService.CreateSearchToken(tokenRequest.ToV4()));
+        }
+    }
+}
---
+++ src/MyLab.Search.Searcher/Models/TokenRequestV2Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+
+namespace MyLab.Search.Searcher.Models
+{
+    static class TokenRequestV2Extensions
+    {
+        public static TokenRequestV4 ToV4(this TokenRequestV2 v2Req)
+        {
+            var req = new TokenRequestV4();
+
+            if (v2Req.Namespaces != null)
+            {
+                req.Indexes = v2Req.Namespaces.Select(ConvertNs).ToArray();
+            }
+
+            return req;
+        }
+
+        private static IndexSettingsV4 ConvertNs(NamespaceSettingsV2 arg)
+        {
+            return new IndexSettingsV4
+            {
+                Id = arg.Name,
+                Filters = arg.Filters
+            };
+        }
+    }
+}
