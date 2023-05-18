6cbf22a791ab728eed29853c4e028ad83ca6a347
Andrey O <ozzy.ext@gmail.com>
2023-05-18 20:39:24 +0300

    Update searcher building

--- ci/publish-sdk.bat
+++ ci/publish-sdk.bat
@@ -2,14 +2,11 @@ echo off
 
 IF [%1]==[] goto noparam
 
-echo "Build image '%1-sdk' and 'latest-sdk'..."
-docker build --progress plain --build-arg DOTNET_BASE=sdk -f ./Dockerfile -t ghcr.io/mylab-search-fx/searcher:%1-sdk -t ghcr.io/mylab-search-fx/searcher:latest-sdk ../src
+echo "Build image '%1-sdk' ..."
+docker build --progress plain --build-arg DOTNET_BASE=sdk -f ./Dockerfile -t ghcr.io/mylab-search-fx/searcher:%1-sdk ../src
 
 echo "Publish image '%1-sdk' ..."
 docker push ghcr.io/mylab-search-fx/searcher:%1-sdk
-
-echo "Publish image 'latest-sdk' ..."
-docker push ghcr.io/mylab-search-fx/searcher:latest-sdk
 
 goto done
 
