7776ed452ab600da73962b162ec3e517db70f28d
Andrey O. <ozzy.ext@gmail.com>
2024-03-29 17:37:13 +0300

    Make publish script short output

--- ci/publish-sdk.bat
+++ ci/publish-sdk.bat
@@ -3,7 +3,7 @@ echo off
 IF [%1]==[] goto noparam
 
 echo "Build image '%1-sdk' ..."
-docker build --progress plain --build-arg DOTNET_BASE=sdk -f ./Dockerfile -t ghcr.io/mylab-search-fx/searcher:%1-sdk ../src
+docker build --build-arg DOTNET_BASE=sdk -f ./Dockerfile -t ghcr.io/mylab-search-fx/searcher:%1-sdk ../src
 
 echo "Publish image '%1-sdk' ..."
 docker push ghcr.io/mylab-search-fx/searcher:%1-sdk
--- ci/publish.bat
+++ ci/publish.bat
@@ -3,7 +3,7 @@ echo off
 IF [%1]==[] goto noparam
 
 echo "Build image '%1' and 'latest'..."
-docker build --progress plain -f ./Dockerfile -t ghcr.io/mylab-search-fx/searcher:%1 -t ghcr.io/mylab-search-fx/searcher:latest ../src
+docker build -f ./Dockerfile -t ghcr.io/mylab-search-fx/searcher:%1 -t ghcr.io/mylab-search-fx/searcher:latest ../src
 
 echo "Publish image '%1' ..."
 docker push ghcr.io/mylab-search-fx/searcher:%1
